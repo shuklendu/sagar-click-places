d4e5a191489c06e2bfc8f14764f2ec8f3d2acb30
Shuklendu Ayachit <shuklendu.ayachit@capgemini.com>
2022-06-28 15:09:01 +0530

    Files added

--- places.js
+++ places.js
@@ -41,7 +41,7 @@ window.onload = () => {
             `latitude: ${latitude}; longitude: ${longitude}`
           );
           icon.setAttribute("name", place.name);
-          icon.setAttribute("src", "./assets/map-marker.png");
+          icon.setAttribute("src", "https://raw.githubusercontent.com/shuklendu/sagar-click-places/main/assets/map-marker.png");
 
           // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
           icon.setAttribute("scale", "20, 20");
